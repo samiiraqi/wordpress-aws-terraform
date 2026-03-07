89c860a732f497736734062b71f5d1807ae08d45
samiiraqi <masdev7979@gmail.com>
2026-03-07 22:06:38 +0200

    Add force_delete to ECR repository

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -3,6 +3,7 @@
 resource "aws_ecr_repository" "wordpress" {
   name                 = "${var.project_name}-php-fpm"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
