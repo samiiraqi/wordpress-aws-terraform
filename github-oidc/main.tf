d9a011c5a235b833dba9bba144318691d48271a8
samiiraqi <masdev7979@gmail.com>
2026-03-24 21:02:08 +0200

    fix: simplify S3 permissions to s3:* on specific buckets

--- github-oidc/main.tf
+++ github-oidc/main.tf
@@ -22,10 +22,7 @@ data "aws_iam_policy_document" "github_actions" {
     sid    = "S3StateAccess"
     effect = "Allow"
     actions = [
-      "s3:GetObject",
-      "s3:PutObject",
-      "s3:DeleteObject",
-      "s3:ListBucket"
+      "s3:*"
     ]
     resources = [
       "arn:aws:s3:::wordpress-terraform-state-156041402173",
@@ -47,34 +44,10 @@ data "aws_iam_policy_document" "github_actions" {
   }
 
   statement {
-    sid    = "S3BucketManagement"
+    sid    = "S3DemoAccess"
     effect = "Allow"
     actions = [
-      "s3:CreateBucket",
-      "s3:DeleteBucket",
-      "s3:GetBucketLocation",
-      "s3:GetBucketVersioning",
-      "s3:PutBucketVersioning",
-      "s3:GetBucketTagging",
-      "s3:PutBucketTagging",
-      "s3:GetBucketPublicAccessBlock",
-      "s3:PutBucketPublicAccessBlock",
-      "s3:GetBucketAcl",
-      "s3:GetBucketObjectLockConfiguration",
-      "s3:GetBucketOwnershipControls",
-      "s3:GetBucketPolicy",
-      "s3:GetBucketRequestPayment",
-      "s3:GetBucketWebsite",
-      "s3:GetEncryptionConfiguration",
-      "s3:GetLifecycleConfiguration",
-      "s3:GetReplicationConfiguration",
-      "s3:ListBucket",
-      "s3:GetObject",
-      "s3:PutObject",
-      "s3:DeleteObject",
-      "s3:GetObjectAcl",
-      "s3:GetObjectTagging",
-      "s3:PutObjectTagging"
+      "s3:*"
     ]
     resources = [
       "arn:aws:s3:::demo-infra-bucket-156041402173",
