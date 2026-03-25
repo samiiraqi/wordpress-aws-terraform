2e71951a13c7d1ab47bc7d6616c3a0f96666d562
samiiraqi <masdev7979@gmail.com>
2026-03-25 19:06:38 +0200

    feat: update CostCenter tag to engineering

--- demo-infra/variables.tf
+++ demo-infra/variables.tf
@@ -17,7 +17,7 @@ variable "tags" {
     Environment = "demo"
     Project     = "demo-infra"
     ManagedBy   = "terraform"
-    CostCenter  = "demo"
+    CostCenter = "engineering"
     Owner       = "samiiraqi"
   }
 }
