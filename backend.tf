7ba7098d09bfd92ff65eb46e3e98ab667bd2cae1
samiiraqi <masdev7979@gmail.com>
2026-03-25 18:27:18 +0200

    refactor: separate backend config into backend.tf

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "wordpress-terraform-state-156041402173"
+    key            = "wordpress/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "wordpress-terraform-lock"
+    encrypt        = true
+    kms_key_id     = "arn:aws:kms:us-east-1:156041402173:key/70f62206-c0c0-49d2-8e96-80965542e33f"
+  }
+}
--- main.tf
+++ main.tf
@@ -1,13 +1,4 @@
 terraform {
-  backend "s3" {
-    bucket         = "wordpress-terraform-state-156041402173"
-    key            = "wordpress/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "wordpress-terraform-lock"
-    encrypt        = true
-    kms_key_id     = "arn:aws:kms:us-east-1:156041402173:key/70f62206-c0c0-49d2-8e96-80965542e33f"
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -35,28 +26,22 @@ module "billing" {
   billing_threshold = 1
 }
 
-
-
-
 module "networking" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.2"
-
   name = "${var.project_name}-vpc"
   cidr = "10.0.0.0/16"
-
   azs             = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.21.0/24", "10.0.22.0/24"]
-
   enable_dns_hostnames = true
   enable_dns_support   = true
   enable_nat_gateway   = false
-
   tags = {
     Name = "${var.project_name}-vpc"
   }
 }
+
 module "security" {
   source       = "./modules/security"
   project_name = var.project_name
@@ -82,5 +67,4 @@ module "compute" {
   db_username               = module.database.db_username
   db_secret_arn             = module.database.db_secret_arn
   sns_topic_arn             = module.billing.sns_topic_arn
-  
 }
