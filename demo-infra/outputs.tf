91a272c1621403228a5dc0b2c820b9709b6bdee1
samiiraqi <masdev7979@gmail.com>
2026-04-09 20:21:16 +0300

    feat: add CI/CD for main infrastructure, remove demo-infra

--- demo-infra/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_name" {
-  description = "Name of the S3 bucket"
-  value       = module.s3_bucket.s3_bucket_id
-}
-
-output "bucket_arn" {
-  description = "ARN of the S3 bucket"
-  value       = module.s3_bucket.s3_bucket_arn
-}
