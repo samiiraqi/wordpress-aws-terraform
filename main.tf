06f356c321d443d1b9bf2f9dfa34e6e09044c1e7
samiiraqi <masdev7979@gmail.com>
2026-04-16 19:46:41 +0300

    style: terraform fmt

--- main.tf
+++ main.tf
@@ -27,16 +27,16 @@ module "billing" {
 }
 
 module "networking" {
-  source               = "terraform-aws-modules/vpc/aws"
-  version              = "5.21.0"
-  name                 = "${var.project_name}-vpc"
-  cidr                 = "10.0.0.0/16"
-  azs                  = ["us-east-1a", "us-east-1b"]
-  public_subnets       = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets      = ["10.0.21.0/24", "10.0.22.0/24"]
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  enable_nat_gateway   = false
+  source                     = "terraform-aws-modules/vpc/aws"
+  version                    = "5.21.0"
+  name                       = "${var.project_name}-vpc"
+  cidr                       = "10.0.0.0/16"
+  azs                        = ["us-east-1a", "us-east-1b"]
+  public_subnets             = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets            = ["10.0.21.0/24", "10.0.22.0/24"]
+  enable_dns_hostnames       = true
+  enable_dns_support         = true
+  enable_nat_gateway         = false
   manage_default_network_acl = false
   tags = {
     Name = "${var.project_name}-vpc"
