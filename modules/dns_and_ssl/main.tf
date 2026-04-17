17840d243588d5b4e759c4bb4d58828047df5082
samiiraqi <masdev7979@gmail.com>
2026-04-17 20:03:14 +0300

    fix: remove duplicate HTTP listener, keep redirect in dns_and_ssl only

--- modules/dns_and_ssl/main.tf
+++ modules/dns_and_ssl/main.tf
@@ -63,20 +63,3 @@ resource "aws_lb_listener" "https" {
   }
 }
 
-# HTTP -> HTTPS Redirect
-resource "aws_lb_listener" "http_redirect" {
-  load_balancer_arn = var.alb_arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
-
