f53aebd7684c7bb663844cf96691f587e1ebf558
samiiraqi <masdev7979@gmail.com>
2026-04-29 19:22:53 +0300

    fix: fix CloudFront certificate ARN dependency

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -139,7 +139,7 @@ module "cloudfront" {
   project_name        = var.project_name
   alb_dns_name        = module.compute.alb_dns_name
   domain_name         = "mywebsitehosting.net"
-  acm_certificate_arn = "arn:aws:acm:us-east-1:156041402173:certificate/59a08c3f-b47e-4dd9-8f8e-027c74b137ff"
+  acm_certificate_arn = module.dns_and_ssl.certificate_arn
   web_acl_arn         = module.waf_cloudfront.web_acl_arn
 }
 
